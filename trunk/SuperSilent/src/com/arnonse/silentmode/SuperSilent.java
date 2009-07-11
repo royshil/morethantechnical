2fe5bca251af48441acaf403d5467e7c3bb982e7
roy.shil <roy.shil@2f01f646-63f4-11de-9867-89bd1bae666e>
2009-07-11 15:22:24 +0000

    added layout to main screen. added dialog for timer, right now connected to start time pick button. timer dialog closes on Done click.

--- trunk/SuperSilent/src/com/arnonse/silentmode/SuperSilent.java
+++ trunk/SuperSilent/src/com/arnonse/silentmode/SuperSilent.java
@@ -2,16 +2,21 @@ package com.arnonse.silentmode;
 
 //import android.R;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
-import android.os.Bundle;
 import android.media.AudioManager;
-
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
-
 public class SuperSilent extends Activity {
+	protected static final int START_TIME_PICKER_DIALOG = 0;
 	private OnCheckedChangeListener m_BasicCheckListener;
 	AudioManager manager = null;
 	
@@ -21,7 +26,7 @@ public class SuperSilent extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cblayout);
 
-        CheckBox cb = ((CheckBox)findViewById(R.id.issilent));
+        CheckBox cb = ((CheckBox)findViewById(R.id.widget58));
 
         manager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
         
@@ -35,8 +40,44 @@ public class SuperSilent extends Activity {
 		
 		cb.setOnCheckedChangeListener(m_BasicCheckListener);
         
-
+		Button myButton = (Button) findViewById(R.id.StartTimeBtn);
+		myButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showDialog(START_TIME_PICKER_DIALOG);
+			}
+		});
     }
+	
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		final Dialog d;
+		switch (id) {
+		case START_TIME_PICKER_DIALOG:
+			AlertDialog.Builder builder;
+
+			Context mContext = getApplicationContext();
+			LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+			View layout = inflater.inflate(R.layout.timerdialog,null);
+
+			builder = new AlertDialog.Builder(this);
+			builder.setView(layout);
+			d = builder.create();
+		
+			Button doneBtn = (Button)layout.findViewById(R.id.SetTimerDoneBtn);
+			doneBtn.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					SuperSilent.this.dismissDialog(START_TIME_PICKER_DIALOG);
+				}
+			});
+			
+			break;
+		default:
+			d = null;
+		}
+		return d;
+	}
     
     @SuppressWarnings("static-access")
 	public void setSilent(boolean silent)
