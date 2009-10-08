4da9a05f3ca8dd66862e565603b1a0372a5bc65f
roy.shil <roy.shil@2f01f646-63f4-11de-9867-89bd1bae666e>
2009-10-08 16:16:05 +0000

    geekcon project 2

--- GeekConRunVas/src/org/geekcon/runvas/Strip.java
+++ GeekConRunVas/src/org/geekcon/runvas/Strip.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.geekcon.runvas.utils.Face;
+import org.geekcon.runvas.utils.Utils;
 import org.geekcon.runvas.utils.Vector3D;
 import org.geekcon.runvas.utils.Vertex;
 
@@ -13,74 +14,102 @@ public class Strip extends DefaultController {
 
 	List<VertexPair> pairs = new ArrayList<VertexPair>();
 	
-	public Strip() {
-		RunVasObject o = new RunVasObject();
-		float count = 10;
-		float halfCount = count/2f;
-		for(float i=0;i<count;i+=1f) {
-			o.loc[0] = (int)(i);
-			o.loc[1] = 0;
-//			o.speed[0] = (int)(halfCount - Math.abs(halfCount-i));
-			o.speed[0] = (int)(Math.sin(i/count * Math.PI) * count);
-			o.speed[1] = 0;
-			o.size = (int)(halfCount - Math.abs(halfCount-i));
-			addToStrip(o);
-			pairs.get(pairs.size()-1).a.vcolor[3] = i/count;
-		}		
+	Vector3D baseColor = new Vector3D(Vector3D.ones);
 
-		m.setLocation(new Vector3D(.5f,.5f,.1f));
+	public Strip() {
+//		RunVasObject o = new RunVasObject();
+//		float count = 50;
+//		float halfCount = count / 2f;
+//		double radius = Math.random() * 100.0;
+//		int startX = (int)(Math.random() * 100.0);
+//		int startY = (int)(Math.random() * 100.0);
+//		baseColor.x = (float)Math.random();
+//		baseColor.y = (float)Math.random();
+//		baseColor.z = (float)Math.random();
+//		for (float i = 0; i < count; i += 1f) {
+//			float rel = i / count;
+//			o.loc[0] = startX + (int)(Math.cos(rel) * radius);
+//			o.loc[1] = startY + (int)(Math.sin(rel) * radius);
+//			// o.speed[0] = (int)(halfCount - Math.abs(halfCount-i));
+//			o.speed[0] = (int) (Math.sin(rel * Math.PI) * 10.0);
+//			o.speed[1] = 0;
+//			o.size = (int) (halfCount - Math.abs(halfCount - i));
+//			addToStrip(o);
+//			pairs.get(pairs.size() - 1).a.vcolor[3] = rel;
+//		}
+
+		m.setLocation(new Vector3D(-1f, -1f, .1f));
 		m.setScale(.01f);
 	}
 
 	public void addToStrip(RunVasObject o) {
-		int x = o.loc[0]; 
+		int x = o.loc[0];
 		int y = o.loc[1];
-		Vector3D objSpeedV = new Vector3D(o.speed[0],o.speed[1],0);
-//		Vector3D objDirV = objSpeedV.getNormalized();
-//		float angle = objDirV.innerProduct(Vector3D.Xaxis);
-		
+		Vector3D objSpeedV = new Vector3D(o.speed[0], o.speed[1], 0);
+		// Vector3D objDirV = objSpeedV.getNormalized();
+		// float angle = objDirV.innerProduct(Vector3D.Xaxis);
+
 		float initialHeight = objSpeedV.getNorm();
-		
-		Vertex a = new Vertex(x,y,initialHeight); 
-		a.vcolor = new float[]{0f,0f,1f,1f}; 
+
+		Vertex a = new Vertex(x, y, initialHeight);
+		a.vcolor = new float[] { baseColor.x, baseColor.y, baseColor.z, 1f };
 		a.setNormal(Vector3D.Xaxis);
-		Vertex b = new Vertex(x,y,initialHeight + o.size); 
-		b.vcolor = a.vcolor; 
+		Vertex b = new Vertex(x, y, initialHeight + o.size);
+		b.vcolor = a.vcolor;
 		b.setNormal(Vector3D.Xaxis);
-		pairs.add(new VertexPair(a,b));
-		
-		if(pairs.size() > 1) {
-		VertexPair pm1 = pairs.get(pairs.size()-2);
-		VertexPair p = pairs.get(pairs.size()-1);
-		m.getQuadFaces().add(
-			Face.createQuadFace(
-					p.a,p.b,pm1.b,pm1.a,
-					Vector3D.Xaxis,false)
-		);
+		pairs.add(new VertexPair(a, b));
+
+		if (pairs.size() > 1) {
+			VertexPair pm1 = pairs.get(pairs.size() - 2);
+			VertexPair p = pairs.get(pairs.size() - 1);
+			m.getQuadFaces().add(
+					Face.createQuadFace(p.a, p.b, pm1.b, pm1.a, Vector3D.Xaxis,
+							false));
+		}
+	}
+
+	private long stripLife = 0l;
+	private boolean stripIsDone;
+	@Override
+	public void advance(long timeElapsed) {
+		super.advance(timeElapsed);
+		stripLife += timeElapsed;
+		float timeElapsedInSec = (float) timeElapsed / 1000f;
+		float decayTimeFactor = 1f - timeElapsedInSec;
+
+		if (pairs.size() > 1) {
+			float[] fs = pairs.get(0).a.vcolor;
+			if (Utils.floatEq(fs[3], 0f)) {
+				pairs.remove(0);
+				m.getQuadFaces().remove(0);
+			}
+			for (int i = 0; i < pairs.size(); i++) {
+				fs = pairs.get(i).a.vcolor;
+				fs[3] *= DECAY_PER_SECOND_FACTOR * decayTimeFactor;
+			}
+		} else {
+			//check how much time has gone past the last update
+			//and kill strip if necessary
+			if(stripLife > 1000l) {
+				//more than a second of no updates - kill strip
+				stripIsDone = true;
+			}
 		}
 	}
 	
 	@Override
-	public void advance(long timeElapsed) {
-		super.advance(timeElapsed);
-		float timeElapsedInSec = 1f - (float)timeElapsed / 1000f;
-		
-		for (int i = 1; i < pairs.size(); i++) {
-			float[] fs = pairs.get(i-1).a.vcolor;
-			fs[3] *= DECAY_PER_SECOND_FACTOR * timeElapsedInSec;
-			fs = pairs.get(i).a.vcolor;
-			fs[3] *= HALF_DECAY_FACTOR * timeElapsedInSec;
-		}
+	public boolean shouldBeRemoved() {
+		return super.shouldBeRemoved() || stripIsDone;
 	}
-	
+
 	class VertexPair {
-		public Vertex a,b;
+		public Vertex a, b;
 
 		public VertexPair(Vertex a, Vertex b) {
 			super();
 			this.a = a;
 			this.b = b;
 		}
-		
+
 	}
 }
