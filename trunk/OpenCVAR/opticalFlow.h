2394770221f1c695142047a14b17659a5e9fcd81
roy.shil <roy.shil@2f01f646-63f4-11de-9867-89bd1bae666e>
2010-11-10 14:56:11 +0000

    none

---
+++ trunk/OpenCVAR/opticalFlow.h
@@ -0,0 +1,14 @@
+/*
+ *
+ *  Created by Roy Shilkrot on 11/3/10.
+ *  Copyright 2010 MIT. All rights reserved.
+ *
+ */
+
+#include <vector>
+
+using namespace std;
+
+#include <opencv.hpp>
+
+using namespace cv;
